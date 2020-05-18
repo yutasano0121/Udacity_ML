e7cb30db24ca02dc028d45928dd71420d0908824
Yuta Asano <yuta@Yutas-MacBook-Pro.local>
2020-05-17 20:18:48 -0500

    format corrected

--- deployment/project_loadData.py
+++ deployment/project_loadData.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import glob
 from tqdm import tqdm
 
@@ -13,21 +12,6 @@ from bs4 import BeautifulSoup
 
 import pickle
 
-
-if not os.path.exists(working_dir + '/data/aclImdb'):
-    subprocess.checl_call(
-        "wget -O ../data/aclImdb_v1.tar.gz http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",
-        shell=True
-    )
-    subprocess.check_call(
-        "tar -zxf ../data/aclImdb_v1.tar.gz -C ../data",
-        shell=True
-    )
-
-
-"""
-Load data.
-"""
 
 def read_imdb_Data(data_dir):
     data = {}
@@ -49,40 +33,24 @@ def read_imdb_Data(data_dir):
                     data[data_type][sentiment].append(review.read())
                     labels[data_type][sentiment].append(
                         1 if sentiment == 'pos' else 0
-                    )  #  Make pos and neg into 1 and 0.
+                    )  # Make pos and neg into 1 and 0.
 
             assert len(data[data_type][sentiment]) == len(labels[data][sentiment]), \
-                "{} / {} data size does not match label size".format(data_type, sentiment)
+                "{} / {} data size does not match label size".format(
+                    data_type, sentiment)
 
     return(data, labels)  # Return a dictionary containing data and labels.
 
 
-data, labels = read_imdb_data(work_dir + 'data/aclImdb')
-logger.info(
-    "Raw data loaded. \n\
-    IMDB reviews: train = {} pos / {} neg, test = {} pos / {} neg".format(
-        len(data['train']['pos']),
-        len(data['train']['neg']),
-        len(data['test']['pos']),
-        len(data['test']['neg'])
-    )
-)
-
-
-"""
-Shuffle the data.
-"""
-
-
 def prepare_imdb_data(data, labels):
     """Prepare training and test sets from IMDb movie reviews."""
-    #Combine positive and negative reviews and labels
+    # Combine positive and negative reviews and labels
     data_train = data['train']['pos'] + data['train']['neg']
     data_test = data['test']['pos'] + data['test']['neg']
     labels_train = labels['train']['pos'] + labels['train']['neg']
     labels_test = labels['test']['pos'] + labels['test']['neg']
 
-    #Shuffle reviews and corresponding labels within training and test sets
+    # Shuffle reviews and corresponding labels within training and test sets
     data_train, labels_train = shuffle(data_train, labels_train)
     data_test, labels_test = shuffle(data_test, labels_test)
 
@@ -100,14 +68,6 @@ logger.info(
 )
 
 
-"""
-Remove HTML tags
-and preprocess words.
-Then pickle the processed data in a cache.
-"""
-
-
-
 def review_to_words(review):
     nltk.download("stopwords", quiet=True)
     stemmer = PorterStemmer()
@@ -115,17 +75,15 @@ def review_to_words(review):
     text = BeautifulSoup(review, "html.parser").get_text()  # Remove HTML tags
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())  # Convert to lower case
     words = text.split()  # Split string into words
-    words = [w for w in words if w not in stopwords.words("english")]  # Remove stopwords
-    words = [PorterStemmer().stem(w) for w in words] # stem
+    words = [w for w in words if w not in stopwords.words(
+        "english")]  # Remove stopwords
+    words = [PorterStemmer().stem(w) for w in words]  # stem
 
     return words
 
 
-os.makedirs(cache_dir)
-
-
 def preprocess_data(data_train, data_test, labels_train, labels_test,
-                    cache_dir=cache_dir, cache_file="preprocessed_data.pkl"):
+                    cache_dir, cache_file):
     """Convert each review to words; read from cache if available."""
 
     # Preprocess training and test data to obtain words for each review
@@ -143,7 +101,3 @@ def preprocess_data(data_train, data_test, labels_train, labels_test,
         print("Wrote preprocessed data to cache file:", cache_file)
 
     return words_train, words_test, labels_train, labels_test
-
-
-train_X, test_X, train_y, test_y = preprocess_data(train_X, test_X, train_y, test_y)
-logger.info('Train and test data preprocessed and pickled in {}'.format(cache_file))
--- deployment/project_main.py
+++ deployment/project_main.py
@@ -5,7 +5,13 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+from project_loadData import read_imdb_Data, prepare_imdb_data, review_to_words, preprocess_data
+from project_makeDict import build_dict
+
+# set working directory
 working_dir = '/home/ec2-user/SageMaker/sagemaker-deployment/'
+
+# set a logger
 log_dir = working_dir + 'log/'
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
@@ -15,7 +21,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:(name)s:%(message)s')
-
 
 file_handler = logging.FileHandler(log_filename)
 print('log file {} created'.format(log_filename))
@@ -32,7 +37,7 @@ if os.path.exists(cache_dir):
     with open(cache_dir + cache_file, "rb") as f:
         cache_data = pickle.load(f)
         # Unpack data loaded from cache file
-        words_train, words_test, labels_train, labels_test = (
+        train_X, test_x, train_y, test_y = (
             cache_data['words_train'],
             cache_data['words_test'],
             cache_data['labels_train'],
@@ -40,13 +45,57 @@ if os.path.exists(cache_dir):
         )
         logger.info("Read preprocessed data from a cache file:", cache_file)
 else:
-    import project_loadData
+    # Download original data if needed
+    if not os.path.exists(working_dir + '/data/aclImdb'):
+        subprocess.checl_call(
+            "wget -O ../data/aclImdb_v1.tar.gz http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",
+            shell=True
+        )
+        subprocess.check_call(
+            "tar -zxf ../data/aclImdb_v1.tar.gz -C ../data",
+            shell=True
+        )
+
+    # Load raw data
+    data, labels = read_imdb_Data(working_dir + 'data/aclImdb')
+    logger.info(
+        "Raw data loaded. \n\
+        IMDB reviews: train = {} pos / {} neg, test = {} pos / {} neg".format(
+            len(data['train']['pos']),
+            len(data['train']['neg']),
+            len(data['test']['pos']),
+            len(data['test']['neg'])
+        )
+    )
+
+    # Shuffle the data.
+    train_X, test_X, train_y, test_y = prepare_imdb_data(data, labels)
+    logger.info(
+        "Data shuffled. \n\
+        sIMDb reviews (combined): train = {}, test = {}".format(
+            len(train_X),
+            len(test_X)
+        )
+    )
+
+    # Preprocess the data and pickle it.
+    train_X, test_X, train_y, test_y = preprocess_data(
+        train_X, test_X, train_y, test_y,
+        cache_dir, cache_file
+    )
+    logger.info('Train and test data preprocessed and pickled in {}'.format(cache_file))
 
 
+# Build a word dictionary.
 cache_file = 'word_dict.pkl'
 if os.path.exists(cache_dir + cache_file):
     with open(cache_dir + cache_file, "rb") as f:
         word_dict = pickle.load(f)
-        logger.info("Read word_dict from a cache file:", cache_file)
+        logger.info("Read word_dict from a cache file: " + cache_file)
 else:
-    import project_makeDict
+    word_dict = build_dict(train_X)
+    logger.info("word_dict constructed.")
+
+    with open(cache_dir + cache_file, "wb") as f:
+        pickle.dump(word_dict, f)
+    logger.info("word_dict pickled: " + cache_file)
